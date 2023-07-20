10aec7b68a1e61b6b2ebc99c805afe4b88259859
Daniel <dmontorfano@hotmail.com>
2023-07-20 09:06:18 -0300

    Lubricacion corrige falta de metodo store en EquipoLubricaionController PC ingenio

--- app/Http/Controllers/EquipoLubricacionController.php
+++ app/Http/Controllers/EquipoLubricacionController.php
@@ -33,7 +33,21 @@ class EquipoLubricacionController extends Controller
 }
 
 
+/**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    
 
+    public function store(Request $request)
+    {  echo"estoy dentrobbbbbbbbbbbbb";
+       return; 
+    }
 
 
     /**
