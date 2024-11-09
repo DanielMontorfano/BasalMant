a66be7ff8dc58eb9a7ac15e306b12f3dfebcb3e6
Daniel <dmontorfano@hotmail.com>
2024-11-09 05:26:25 -0300

    corregido foto en storage

--- app/Http/Controllers/imprimirController.php
+++ app/Http/Controllers/imprimirController.php
@@ -8,6 +8,7 @@ use App\Models\Protocolo;
 use App\Models\Equipoplansejecut; 
 use App\Models\Tareash;
 use App\Models\Repuesto;
+use App\Models\Foto;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -49,20 +50,22 @@ $dompdf->stream(); */
         
     
                 
-               $equipo= Equipo::find($id); // Ver la linea de abajo alternativa 
-               $repuestos=$equipo->equiposRepuestos;
-               $plans=Equipo::find($id)->equiposPlans; 
-               $equiposB=Equipo::find($id)->equiposAEquiposB; 
-               $pdf = PDF::loadView('impresiones.imprimirFichaEquipo', compact('equipo', 'repuestos', 'plans','equiposB'));
-               $variable="Ficha " . $equipo->codEquipo .".pdf";
-               return $pdf->download($variable); 
-               
-              //return $equipo; 
-              //return view('imprimir'); 
-            //  return view('imprimir2', compact('equipo'));
-             // return  view('imprimir');
-       
-           }
+      $equipo= Equipo::find($id); // Ver la linea de abajo alternativa 
+      $repuestos=$equipo->equiposRepuestos;
+      $plans=Equipo::find($id)->equiposPlans; 
+      $equiposB=Equipo::find($id)->equiposAEquiposB; 
+      $primerRutaFoto = Foto::where('equipo_id', $equipo->id)->value('rutaFoto');
+     // return $primerRutaFoto;
+      $pdf = PDF::loadView('impresiones.imprimirFichaEquipo', compact('equipo', 'repuestos', 'plans','equiposB','primerRutaFoto'));
+      $variable="Ficha " . $equipo->codEquipo .".pdf";
+      return $pdf->download($variable); 
+      
+     //return $equipo; 
+     //return view('imprimir'); 
+   //  return view('imprimir2', compact('equipo'));
+    // return  view('imprimir');
+
+  }
     
            public function imprimirOrden($id){
         
